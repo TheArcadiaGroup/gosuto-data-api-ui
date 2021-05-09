2d1ad1be285134c6311ac66bea7372c0acdb22c5
YessineAmor <yassine.amor@esprit.tn>
2021-05-09 03:49:31 +0100

    update pricing css

--- src/views/admin/Pricing.js
+++ src/views/admin/Pricing.js
@@ -19,7 +19,7 @@ export default function Pricing() {
   return (
     <>
       <div
-        className=" bg-blueGray-700 h-full py-16 antialiased bg-primary-very-light font-sans"
+        className="  h-full py-16 antialiased bg-primary-very-light font-sans"
         style={{ marginTop: 150 }}
       >
         <div
@@ -27,17 +27,18 @@ export default function Pricing() {
           class="flex flex-col  items-center lg:justify-center w-full w-full lg:px-10 py-12"
         ></div>
         <div className="flex flex-col items-center mb-12 text-white">
-          <h2 className="text-3xl   font-bold">Our Pricing</h2>
+          <h2 className="text-3xl font-bold text-black">Our Pricing</h2>
         </div>
 
-        <section className="flex flex-col  lg:flex-row items-start items-center lg:justify-center w-full w-full lg:px-10 py-12">
+        <section className="flex flex-col lg:flex-row items-start items-center lg:justify-center w-full w-full lg:px-10 py-12 mr-10">
           {packs.map((pack) => {
             if (pack._id === user.pack._id) {
               return (
                 <article
-                  className="w-full  bg-white lg:w-custom w-4/5 mb-10 px-6 py-16 lg:-mt-6 text-white text-center rounded-lg"
+                  className="filter drop-shadow w-full bg-white lg:w-custom w-4/5 mb-10 px-6 py-16 lg:-mt-6 text-white text-center rounded-lg filter drop-shadow"
                   style={{
-                    backgroundImage: 'linear-gradient(90deg, #70aac7 0%, #0284c7 100%)'
+                    backgroundImage: 'linear-gradient(90deg, #70aac7 0%, #0284c7 100%)',
+                    marginRight: '20px'
                   }}
                   key={`pricing_${pack._id}`}
                 >
@@ -77,7 +78,10 @@ export default function Pricing() {
             } else {
               return (
                 <article
-                  className="w-full bg-white w-4/5 lg:w-custom mb-10 lg:px-4 px-6 py-10 text-center text-primary-dark rounded-lg"
+                  className="mr-20 w-full bg-white w-4/5 lg:w-custom mb-10  lg:px-4 px-6 py-10 text-center text-primary-dark rounded-lg"
+                  style={{
+                    marginRight: '20px'
+                  }}
                   key={`pricing_${pack._id}`}
                 >
                   <h5 className="font-bold text-base">{pack.name}</h5>

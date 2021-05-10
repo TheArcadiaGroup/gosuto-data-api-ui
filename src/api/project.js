c49986db3bae39bc165adb399cac38004bd4a0b5
seifeddine brahem <brhseif@gmail.com>
2021-05-10 03:57:17 +0100

    Edit/delete/RegenrateApiKey project

--- src/api/project.js
+++ src/api/project.js
@@ -1,7 +1,17 @@
 import http from '../services/httpService'
 
 const postProject = (data) => http.post(`/project`, data)
-const regenrateApiKey = (id) => http.put(`/project/${id}`)
+const regenrateApiKey = (id) => http.put(`/project/regenrateApiKey/${id}`)
+const editProject = (data, id) => http.put(`/project/${id}`, data)
 const getProjects = () => http.get(`/project`)
+const getProject = (id) => http.get(`/project/${id}`)
+const deleteProject = (id) => http.delete(`/project/${id}`)
 
-export { postProject, getProjects, regenrateApiKey }
+export {
+  postProject,
+  getProjects,
+  regenrateApiKey,
+  deleteProject,
+  getProject,
+  editProject
+}
